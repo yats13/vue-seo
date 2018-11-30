1b5b481143b1191055bbd4b80aef3c5dfd41412e
yats13 <yatskovsky@mail.ru>
2018-11-30 15:22:31 +0200

    v. 1

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/" exact>Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <transition name="fade">
+      <router-view/>
+    </transition>
+
+  </div>
+</template>
+
+<style lang="scss">
+  @import '@/scss/base.scss';
+</style>
---
+++ src/router.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      meta: {
+        title: 'Home Page - Example App',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'The home page of our example app.'
+          },
+          {
+            property: 'og:description',
+            content: 'The home page of our example app.'
+          }
+        ]
+      }
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      meta: {
+        title: 'Ab Page - Example App',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'The home page of our example app.'
+          },
+          {
+            property: 'og:description',
+            content: 'The home page of our example app.'
+          }
+        ]
+      }
+    }
+  ],
+  mode: 'history',
+})
